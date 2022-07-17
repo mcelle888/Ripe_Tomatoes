7767f59fe087c29cb7368c55c20b2ad0fae10eb5
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2022-07-17 22:54:42 +1000

    challenge completed

--- README.md
+++ README.md
@@ -18,9 +18,13 @@ If everything works fine let's go with the database creation:
 Back to the flask application these are tesks you need to to to complete the challenge:
 
 - create the connection to the database you have just created. (Remember to install all the needed packages and then to store them in requirements.txt)
-- Create the Movie and Actor models in separate files from app.py. From each movie we'd like to store the id, title, genre, length(in minutes) and release year. From each actor we'd like to store the id, first and last name, gender, country and date of birth. Feel free to add more attributes to each model, but don't add relationships between movies and actors, we'll work on this in further lessons.
+- Create the Movie and Actor models. From each movie we'd like to store the id, title, genre, length(in minutes) and release year. From each actor we'd like to store the id, first and last name, gender, country and date of birth. Feel free to add more attributes to each model, but don't add relationships between movies and actors, we'll work on this in further lessons.
 - Create the cli commands that create and drop the tables.
 - Create the cli command that seeds both tables. Seed them with at least two movies and 4 actors
 - Create two routes ```/movies``` and ```/actors``` that print the information stored in the database in JSON format when there is a GET method request.
 
 Challenge completed when these 3 routes show the following information (of course, movies and actors information don't match with your data.)
+
+![postman screenshot with Welcome message](screenshots/root_get.png)
+![postman screenshot with movies data](screenshots/movies_get.png)
+![postman screenshot with actors data](screenshots/actors_get.png)
--- app.py
+++ app.py
@@ -1,6 +1,132 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+from flask_marshmallow import Marshmallow
+ma = Marshmallow(app)
+
+## DB CONNECTION AREA
+
+from flask_sqlalchemy import SQLAlchemy 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://tomato:123456@localhost:5432/ripe_tomatoes_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# CLI COMMANDS AREA
+
+@app.cli.command("create")
+def create_db():
+    db.create_all()
+    print("Tables created")
+
+@app.cli.command("seed")
+def seed_db():
+
+    movie1 = Movie(
+        title = "Spider-Man: No Way Home",
+        genre = "Action",
+        length = 148,
+        year = 2021
+    )
+    db.session.add(movie1)
+
+    movie2 = Movie(
+        title = "Dune",
+        genre = "Sci-fi",
+        length = 155,
+        year = 2021
+    )
+    db.session.add(movie2)
+
+    actor1 = Actor(
+        first_name = "Tom",
+        last_name = "Holland",
+        gender = "male",
+        country = "UK"
+    )
+    db.session.add(actor1)
+
+    actor2 = Actor(
+        first_name = "Marisa",
+        last_name = "Tomei",
+        gender = "female",
+        country = "USA"
+    )
+    db.session.add(actor2)
+
+    actor3 = Actor(
+        first_name = "Timothee",
+        last_name = "Chalemet",
+        gender = "male",
+        country = "USA"
+    )
+    db.session.add(actor3)
+
+    actor4 = Actor(
+        first_name = "Zendaya",
+        last_name = "",
+        gender = "female",
+        country = "USA"
+    )
+    db.session.add(actor4)
+   
+    db.session.commit()
+    print("Tables seeded") 
+
+@app.cli.command("drop")
+def drop_db():
+    db.drop_all()
+    print("Tables dropped") 
+
+# MODELS AREA
+
+class Movie(db.Model):
+    __tablename__= "MOVIES"
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String())
+    genre = db.Column(db.String())
+    length = db.Column(db.Integer())
+    year = db.Column(db.Integer())
+
+class Actor(db.Model):
+    __tablename__= "ACTORS"
+    id = db.Column(db.Integer,primary_key=True)  
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    gender = db.Column(db.String())
+    country = db.Column(db.String())
+
+# SCHEMAS AREA
+
+class MovieSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "genre", "length", "year")
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+class ActorSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "first_name", "last_name", "gender", "country")
+
+actor_schema = ActorSchema()
+actors_schema = ActorSchema(many=True)
+
+
+# ROUTING AREA
 
 @app.route("/")
 def hello():
   return "Welcome to Ripe Tomatoes API"
+
+@app.route("/movies", methods=["GET"])
+def get_movies():
+    movies_list = Movie.query.all()
+    result = movies_schema.dump(movies_list)
+    return jsonify(result)
+
+@app.route("/actors", methods=["GET"])
+def get_actors():
+    actors_list = Actor.query.all()
+    result = actors_schema.dump(actors_list)
+    return jsonify(result)
